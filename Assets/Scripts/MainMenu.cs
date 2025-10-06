afae1fdb3036e012e437911aec1e11dbeb59b522
ermessondev <ermesson404@gmail.com>
2025-10-06 10:52:37 -0300

    Scene Treinamento
    - Adicionado Scene de treinamento

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,17 +6,21 @@ public class MainMenu : MonoBehaviour
 {
     // Todos os Debug.Log são de teste por enquanto 
     [SerializeField] Button singlePlayer;
+    [SerializeField] Button treinamento;
     [SerializeField] Button multiPlayer;
+    
     void Start()
     {
-        singlePlayer.onClick.AddListener(() => modoDeJogo(true));
-        multiPlayer.onClick.AddListener(() => modoDeJogo(false));
+        singlePlayer.onClick.AddListener(() => modoDeJogo(true, "CharacterSelect"));
+        treinamento.onClick.AddListener(() => modoDeJogo(true, "Treinamento"));
+        multiPlayer.onClick.AddListener(() => modoDeJogo(false, "CharacterSelect"));
+
     }
 
-    void modoDeJogo (bool modoDeJogo) 
+    void modoDeJogo (bool modoDeJogo, string scene) 
     { 
         GameManager.instance.singleMode = modoDeJogo;
-        SceneManager.LoadScene("CharacterSelect");
+        SceneManager.LoadScene(scene);
     }
     void Update()
     {
